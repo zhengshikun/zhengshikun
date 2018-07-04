211e169683d87241df3f417a2fbc894019074a0c
zheng shi kun <646537311@qq.com>
2018-07-04 16:18:08 +0800

    blog7.4

--- src/router/index.js
+++ src/router/index.js
@@ -52,5 +52,15 @@ export default new Router({
       title: '想看看我吗'
     }
   }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return {
+        x: 0,
+        y: 0
+      }
+    }
+  }
 })
